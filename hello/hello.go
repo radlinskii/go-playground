35331c66617b02ce0e2fa55bd8b9e8253b5baef0
agnieszka-miszkurka <miszkur@student.agh.edu.pl>
2018-09-06 15:25:40 +0200

    Added package import and usage

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/radlinskii/go-playground/stringutil"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Printf(stringutil.Reverse("\n!oG ,olleH"))
 }
