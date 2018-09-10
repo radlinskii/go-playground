c5ee9d0964c9cf7a5f5885714379129c0c64af06
agnieszka-miszkurka <miszkur@student.agh.edu.pl>
2018-09-10 10:21:48 +0200

    Updated hello.go function calls

--- hello/hello.go
+++ hello/hello.go
@@ -11,30 +11,33 @@ import (
 
 func main() {
 	fmt.Printf("reversed message: %s\n", string_utils.Reverse("!oG ,olleH"))
+
 	fmt.Printf("uppercase message: %s\n", string_utils.ToUpperCase("Hello, Go!"))
 
 	var num float64 = -3
 	sqrt, err := number_utils.Sqrt(num)
 	if err != nil {
-        fmt.Print(err)
+        fmt.Printf("Error: %s", err)
     } else {
         fmt.Printf("my sqrt of %g: %g\n", num, sqrt)
     }
-	fmt.Printf("is %d a odd number? %t\n", int(num), number_utils.IsOdd(int(num)))
-	file_utils.Copy("../file_utils/new_file.txt","../file_utils/file.txt")
-	fmt.Println(string_utils.WordCount("Hello World!! World! I said Hello !!\n"))
-	number_utils.PrintFibonacci()
+
+    fmt.Printf("is %d a odd number? %t\n", int(num), number_utils.IsOdd(int(num)))
+
+    file_utils.Copy("../file_utils/new_file.txt","../file_utils/file.txt")
+
+    fmt.Println(string_utils.WordCount("Hello World!! World! I said Hello !!\n"))
+
+    number_utils.PrintFibonacci()
 
 	var name string
 	flag.StringVar(&name, "name", "", "Who should i greet?")
     flag.Parse()
-
     if name == "" {
         fmt.Println("Hello World!")
     } else {
         fmt.Printf("Hello %s!\n", name)
     }
-
 
     if len(os.Args) == 2 {
         file_utils.Write(os.Args[1])
