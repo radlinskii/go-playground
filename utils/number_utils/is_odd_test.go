c82e6470c0921d72f1db57c6486ad2374cec28de
radlinskii <radlinskiignacy@gmail.com>
2018-09-11 21:55:08 +0200

    Created a test for IsOdd function

---
+++ utils/number_utils/is_odd_test.go
@@ -0,0 +1,25 @@
+package number_utils
+
+import "testing"
+
+var tests = map[int]bool{
+	3: true,
+	2: false,
+	1: true,
+	0: false,
+	4: false,
+	5: true,
+}
+
+func TestIsOdd(t *testing.T) {
+	for key, expectedVal := range tests {
+		val := IsOdd(key)
+		if val != expectedVal {
+			t.Error(
+				"For", key,
+				"expected", expectedVal,
+				"got", val,
+			)
+		}
+	}
+}
