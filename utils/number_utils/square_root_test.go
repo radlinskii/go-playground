da1303de852bd75c9c69c4533a61b676fd046a8d
radlinskii <radlinskiignacy@gmail.com>
2018-09-12 23:19:57 +0200

    Updated sqrt_test to use table driven tests

--- utils/number_utils/square_root_test.go
+++ utils/number_utils/square_root_test.go
@@ -6,53 +6,60 @@ import (
 )
 
 func TestSqrt(t *testing.T) {
-	var tests = map[float64]float64{
-		3:  math.Sqrt(3),
-		2:  math.Sqrt(2),
-		1:  math.Sqrt(1),
-		0:  math.Sqrt(0),
-		4:  math.Sqrt(4),
-		5:  math.Sqrt(5),
-		-5: 0,
-		-1: 0,
+	var testsTable = []struct {
+		name     string
+		number   float64
+		expected float64
+	}{
+		{"positive number #1", 13, math.Sqrt(13)},
+		{"positive number #2", 31, math.Sqrt(31)},
+		{"zero", 0, 0},
+		{"negative number #1", -11, 0},
+		{"negative number #2", -1, 0},
 	}
 	precision := 0.00000001
 
-	for key, expectedVal := range tests {
-		val, _ := Sqrt(key)
-		if val < expectedVal-precision || val > expectedVal+precision {
-			t.Error(
-				"For", key,
-				"expected", expectedVal,
-				"got", val,
-			)
-		}
+	for _, test := range testsTable {
+		t.Run(test.name, func(t *testing.T) {
+			got, _ := Sqrt(test.number)
+			if test.number > 0 {
+				if got < test.expected-precision || got > test.expected+precision {
+					t.Errorf("Sqrt(%g) got %g expected %g", test.number, got, test.expected)
+				}
+			} else {
+				if got != test.expected {
+					t.Errorf("Sqrt(%g) got %g expected %g", test.number, got, test.expected)
+				}
+			}
+		})
 	}
 }
 
 func TestErrNegativeSqrt_Error(t *testing.T) {
-	tests := [8]float64{-3.5, -3.4, -2, -1, 0, 1, 2, 5}
+	testsTable := []struct {
+		name   string
+		number float64
+	}{
+		{"positive number #1", 13},
+		{"positive number #2", 31},
+		{"zero", 0},
+		{"negative number #1", -11},
+		{"negative number #2", -1},
+	}
 
-	for _, testNumber := range tests {
-		_, err := Sqrt(testNumber)
+	for _, test := range testsTable {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := Sqrt(test.number)
 
-		if testNumber < 0 {
-			_, ok := err.(ErrNegativeSqrt)
-			if !ok {
-				t.Error(
-					"For", testNumber,
-					"expected ErrNegativeSqrt",
-					"got", err,
-				)
+			if test.number >= 0 {
+				if err != nil {
+					t.Errorf("For %g expected <nil> got %v", test.number, err)
+				}
+			} else {
+				if err != ErrNegativeSqrt(test.number) {
+					t.Errorf("For %g expected %v got %v", test.number, ErrNegativeSqrt(test.number), err)
+				}
 			}
-		} else {
-			if err != nil {
-				t.Error(
-					"For", testNumber,
-					"expected <nil>",
-					"got", err,
-				)
-			}
-		}
+		})
 	}
 }
