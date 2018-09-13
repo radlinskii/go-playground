cca8e4d5d1e8f958710a11d189042abfa82f15bf
radlinskii <radlinskiignacy@gmail.com>
2018-09-13 18:23:25 +0200

    Created ExamplePrintRot13Encoded test

---
+++ utils/string_utils/rotate13_test.go
@@ -0,0 +1,13 @@
+package string_utils
+
+func ExamplePrintRot13Encoded() {
+	// TODO PrintRot13Encoded("")
+	PrintRot13Encoded("\n")
+	PrintRot13Encoded("hello\n")
+	PrintRot13Encoded("HELLO\n")
+	PrintRot13Encoded("Hello!\n")
+	// Output:
+	// uryyb
+	// URYYB
+	// Uryyb!
+}
