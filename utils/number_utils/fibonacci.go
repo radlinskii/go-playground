43c9d4792efd434296a0bbbaca8142bd3543bd5e
radlinskii <radlinskiignacy@gmail.com>
2018-09-14 11:48:33 +0200

    Renamed PrintFibonacci to PrintFibonacciWithClosure

--- utils/number_utils/fibonacci.go
+++ utils/number_utils/fibonacci.go
@@ -2,7 +2,7 @@ package number_utils
 
 import "fmt"
 
-func fibonacci() func() int {
+func fibonacciWithClosure() func() int {
 	i, a, b := 0, 0, 1
 	return func() int {
 		i = a
@@ -12,9 +12,9 @@ func fibonacci() func() int {
 	}
 }
 
-// PrintFibonacci prints first 10 Fibonacci numbers.
-func PrintFibonacci() {
-	f := fibonacci()
+// PrintFibonacciWithClosure prints first 10 Fibonacci numbers.
+func PrintFibonacciWithClosure() {
+	f := fibonacciWithClosure()
 	fmt.Print("first 10 of Fibonacci numbers: ")
 	for i := 0; i < 10; i++ {
 		fmt.Printf("%d ", f())
--- utils/number_utils/fibonacci_test.go
+++ utils/number_utils/fibonacci_test.go
@@ -1,6 +1,6 @@
 package number_utils
 
-func ExamplePrintFibonacci() {
-	PrintFibonacci()
+func ExamplePrintFibonacciWithClosure() {
+	PrintFibonacciWithClosure()
 	// Output: first 10 of Fibonacci numbers: 0 1 1 2 3 5 8 13 21 34
 }
