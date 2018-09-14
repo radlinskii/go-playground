f709361ee3506403c4745d9984c7a82b9402598d
radlinskii <radlinskiignacy@gmail.com>
2018-09-14 21:27:26 +0200

    Updated Fibonacci functions to handle n lte zero

--- utils/number_utils/fibonacci.go
+++ utils/number_utils/fibonacci.go
@@ -1,6 +1,12 @@
 package number_utils
 
-// TODO handle n == 0 && n < 0
+import "fmt"
+
+type errNegativeAmount float64
+
+func (e errNegativeAmount) Error() string {
+	return fmt.Sprintf("cannot operate on negative amount of numbers: %g", float64(e))
+}
 
 func getFibonacciWithClosure() func() int {
 	i, a, b := 0, 0, 1
@@ -14,15 +20,21 @@ func getFibonacciWithClosure() func() int {
 
 // FibonacciWithClosure returns a slice of first n Fibonacci numbers
 // implemented using closures.
-func FibonacciWithClosure(n int) []int {
-	s := make([]int, n)
+func FibonacciWithClosure(n int) ([]int, error) {
+	if n < 0 {
+		return []int{}, errNegativeAmount(n)
+	}
+	if n == 0 {
+		return []int{}, nil
+	}
+	var s []int
 
 	f := getFibonacciWithClosure()
 
 	for i := 0; i < n; i++ {
 		s = append(s, f())
 	}
-	return s
+	return s, nil
 }
 
 func getFibonacciWithChannel(n int, c chan int) {
@@ -36,8 +48,14 @@ func getFibonacciWithChannel(n int, c chan int) {
 
 // FibonacciWithChannel returns a slice of first n Fibonacci numbers
 // implemented using goroutine & channel.
-func FibonacciWithChannel(n int) []int {
-	s := make([]int, n)
+func FibonacciWithChannel(n int) ([]int, error) {
+	if n < 0 {
+		return []int{}, errNegativeAmount(n)
+	}
+	if n == 0 {
+		return []int{}, nil
+	}
+	var s []int
 	c := make(chan int, n)
 
 	go getFibonacciWithChannel(cap(c), c)
@@ -45,5 +63,5 @@ func FibonacciWithChannel(n int) []int {
 	for i := range c {
 		s = append(s, i)
 	}
-	return s
+	return s, nil
 }
