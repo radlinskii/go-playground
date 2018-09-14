adbc4286bc2fda3114bf224ea0ef781144041d1c
radlinskii <radlinskiignacy@gmail.com>
2018-09-14 10:58:33 +0200

    Removed Write function

--- utils/utils.go
+++ utils/utils.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"os"
-
 	"github.com/radlinskii/go-playground/utils/file_utils"
 )
 
 func main() {
-	if len(os.Args) == 2 {
-		file_utils.Write(os.Args[1])
-	}
-
 	file_utils.RemoveTabs("./file_utils/file.txt")
 }
