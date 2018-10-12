128d6866b5380611f19cba9ebd74dfd7e8011251
radlinskii <radlinskiignacy@gmail.com>
2018-10-12 10:37:56 +0200

    Create MakeNode method test

--- datastructures/heaps/fibonacci/fibonacci_test.go
+++ datastructures/heaps/fibonacci/fibonacci_test.go
@@ -13,3 +13,24 @@ func TestMakeHeap(t *testing.T) {
 		t.Error("New FibonacciHeap should have 0 as a number of elements.")
 	}
 }
+
+func TestMakeNode(t *testing.T) {
+	expected := 4
+	n := MakeNode(expected)
+
+	if n.key != expected {
+		t.Errorf("Expected Node Key: %d, got %d", expected, n.key)
+	}
+
+	if n.parent != nil || n.child != nil || n.left != nil || n.right != nil {
+		t.Error("New Node should have nil as its pointers")
+	}
+
+	if n.mark != false {
+		t.Error("New Node should be unmarked")
+	}
+
+	if n.degree != 0 {
+		t.Errorf("New Node degree should be 0, got: %d", n.degree)
+	}
+}
