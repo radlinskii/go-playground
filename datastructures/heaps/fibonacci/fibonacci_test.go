1da9d4449b9089d5fbd5150fd68c755989056fba
radlinskii <radlinskiignacy@gmail.com>
2018-10-12 10:38:58 +0200

    Style test failing messages

--- datastructures/heaps/fibonacci/fibonacci_test.go
+++ datastructures/heaps/fibonacci/fibonacci_test.go
@@ -6,11 +6,11 @@ func TestMakeHeap(t *testing.T) {
 	h := MakeHeap()
 
 	if h.min != nil {
-		t.Error("New FibonacciHeap should have nil as a pointer to the minimum element from its rootlist.")
+		t.Error("New FibonacciHeap should have nil as a pointer to the minimum element from its rootlist")
 	}
 
 	if h.n != 0 {
-		t.Error("New FibonacciHeap should have 0 as a number of elements.")
+		t.Error("New FibonacciHeap should have 0 as a number of elements")
 	}
 }
 
