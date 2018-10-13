75eca3706cb16c076ba13f56ec599b1859ebdbf6
Radliński Ignacy <radlinskiignacy@gmail.com>
2018-10-13 08:43:04 +0200

    implement Fibonacci heap (#7)
    * Implement Fibonacci Heap data structure
    
    * Update structure of datastructures package
    
    * Change fibonacci.Node.Key to be of type int
    
    * Simplify MakeHeap method
    
    * Remove fibonacciHeap.go
    
    * Remove duplicated code
    
    * Extract function removing node from a list
    
    * Make Node.Key unexported
    
    * Create a exported MakeNode method
    
    * Make GetKey getter exported
    
    * Create test for MakeHeap method
    
    * Create MakeNode method test
    
    * Style test failing messages
    
    * Create GetKey method test
    
    * Create addNode function test
    
    * Create addRoot method test
    
    * Compose methods order
    
    * Create removeNodeFromList function test
    
    * Create Insert method test
    
    * Create Minimum method test
    
    * Create Union method test
    
    * Change link to be function instead of method
    
    * Create link function test
    
    * Create consolidate method test
    
    * Remove unnecessary blank lines
    
    * Refactor DecreaseKey to return an error
    
    * Create ExtractMin method test
    
    * Update errInvalidArgument message
    
    * Create errInvalidArgument Error test
    
    * Create cut method test
    
    * Create cascadingCut method test
    
    * Remove Vis method
    
    * Update ExtractMin test to cover more code
    
    * Create DecreaseKey method test
    
    * Create Delete method test
    
    * Rename Heap receiver
    
    * Add Package documentation

---
+++ datastructures/heaps/fibonacci/fibonacci.go
@@ -0,0 +1,234 @@
+// Package fibonacci is an implementation from Introduction to Algorithms by T. Cormen
+// Reference: https://en.wikipedia.org/wiki/Fibonacci_heap
+package fibonacci
+
+import "fmt"
+
+// Heap is a implementation of Fibonacci heap.
+type Heap struct {
+	min *Node
+	n   int
+}
+
+// Node holds structure of nodes inside Fibonacci heap.
+type Node struct {
+	key                        int
+	left, right, parent, child *Node
+	mark                       bool
+	degree                     int
+}
+
+// GetKey gets a value of node's key.
+func (n *Node) GetKey() int {
+	return n.key
+}
+
+// MakeNode creates a node with key equal to the given value.
+func MakeNode(k int) *Node {
+	return &Node{key: k}
+}
+
+func addNode(n1, n2 *Node) {
+	n1.left.right = n2
+	n2.right = n1
+	n2.left = n1.left
+	n1.left = n2
+}
+
+func (h *Heap) addRoot(x *Node) {
+	if h.min == nil {
+		// create h's root list containing only x
+		x.left = x
+		x.right = x
+		h.min = x
+	} else {
+		// insert x to h's root list
+		addNode(h.min, x)
+		if x.key < h.min.key {
+			h.min = x
+		}
+	}
+}
+
+func removeNodeFromList(n *Node) {
+	n.left.right = n.right
+	n.right.left = n.left
+}
+
+// MakeHeap creates and returns a new, empty heap.
+func MakeHeap() *Heap {
+	return &Heap{}
+}
+
+// Insert inserts a new node, with predeclared key, to the heap.
+func (h *Heap) Insert(x *Node) *Node {
+	x.degree = 0
+	x.mark = false
+	x.parent = nil
+	x.child = nil
+
+	h.addRoot(x)
+	h.n++
+	return x
+}
+
+// Minimum returns pointer to the heap's node holding the minimum key.
+func (h *Heap) Minimum() *Node {
+	return h.min
+}
+
+// Union creates and returns the merge of two mergeable heaps.
+func (h *Heap) Union(fh2 *Heap) *Heap {
+	newFH := MakeHeap()
+	newFH.min = h.min
+
+	newFH.min.left.right = fh2.min
+	fh2.min.left.right = newFH.min
+	fh2.min.left, newFH.min.left = newFH.min.left, fh2.min.left
+
+	if h.min == nil || (fh2.min != nil && h.min.key > fh2.min.key) {
+		newFH.min = fh2.min
+	}
+	newFH.n = h.n + fh2.n
+	return newFH
+}
+
+// ExtractMin extracts the node with minimum key from a heap
+// and returns pointer to this node.
+func (h *Heap) ExtractMin() *Node {
+	z := h.min
+	if z != nil {
+		for {
+			// add z children to h's root list
+			if x := z.child; x != nil {
+				x.parent = nil
+				if x.right != x {
+					z.child = x.right
+					removeNodeFromList(x)
+				} else {
+					z.child = nil
+				}
+				addNode(z, x)
+			} else {
+				break
+			}
+		}
+		removeNodeFromList(z)
+
+		if z == z.right {
+			h.min = nil
+		} else {
+			h.min = z.right
+			h.consolidate()
+		}
+		h.n--
+	}
+	return z
+}
+
+func (h *Heap) consolidate() {
+	degreeToRoot := make(map[int]*Node)
+	w := h.min
+	last := w.left
+	for {
+		r := w.right
+		x := w
+		d := x.degree
+		for {
+			if y, ok := degreeToRoot[d]; !ok {
+				break
+			} else {
+				if y.key < x.key {
+					y, x = x, y
+				}
+				link(y, x)
+				delete(degreeToRoot, d)
+				d++
+			}
+		}
+		degreeToRoot[d] = x
+		if w == last {
+			break
+		}
+		w = r
+	}
+	h.min = nil
+	for _, v := range degreeToRoot {
+		h.addRoot(v)
+	}
+}
+
+func link(y, x *Node) {
+	removeNodeFromList(y)
+	// make y a child of x and increase degree of x
+	y.parent = x
+	if x.child == nil {
+		x.child = y
+		y.left = y
+		y.right = y
+	} else {
+		addNode(x.child, y)
+	}
+
+	y.mark = false
+}
+
+type errInvalidArgument int
+
+func (e errInvalidArgument) Error() string {
+	return fmt.Sprintf("Cannot decrese key to a bigger value: %d", int(e))
+}
+
+// DecreaseKey decreases the key of given node.
+func (h *Heap) DecreaseKey(x *Node, k int) error {
+	if x.key < k {
+		return errInvalidArgument(k)
+	}
+	x.key = k
+	y := x.parent
+	if y != nil && x.key < y.key {
+		h.cut(x, y)
+		h.cascadingCut(y)
+	}
+	if x.key < h.min.key {
+		h.min = x
+	}
+	return nil
+}
+
+func (h *Heap) cut(x, y *Node) {
+	// remove x from y's children list and decrement y's degree
+	if x.right != x {
+		y.child = x.right
+		removeNodeFromList(x)
+	} else {
+		y.child = nil
+	}
+	y.degree--
+	addNode(h.min, x)
+
+	x.parent = nil
+	x.mark = false
+}
+
+func (h *Heap) cascadingCut(y *Node) {
+	z := y.parent
+	if z != nil {
+		if !y.mark {
+			y.mark = true
+		} else {
+			h.cut(y, z)
+			h.cascadingCut(z)
+		}
+	}
+}
+
+// Delete deletes node x from heap h.
+func (h *Heap) Delete(x *Node) error {
+	err := h.DecreaseKey(x, int(-1<<63))
+	if err != nil {
+		return err
+	}
+	h.ExtractMin()
+	return nil
+}
---
+++ datastructures/heaps/fibonacci/fibonacci_test.go
@@ -0,0 +1,442 @@
+package fibonacci
+
+import "testing"
+
+func TestMakeHeap(t *testing.T) {
+	h := MakeHeap()
+
+	if h.min != nil {
+		t.Error("New FibonacciHeap should have nil as a pointer to the minimum element from its rootlist")
+	}
+
+	if h.n != 0 {
+		t.Error("New FibonacciHeap should have 0 as a number of elements")
+	}
+}
+
+func TestMakeNode(t *testing.T) {
+	expected := 4
+	n := MakeNode(expected)
+
+	if n.key != expected {
+		t.Errorf("Expected Node Key: %d, got %d", expected, n.key)
+	}
+
+	if n.parent != nil || n.child != nil || n.left != nil || n.right != nil {
+		t.Error("New Node should have nil as its pointers")
+	}
+
+	if n.mark != false {
+		t.Error("New Node should be unmarked")
+	}
+
+	if n.degree != 0 {
+		t.Errorf("New Node degree should be 0, got: %d", n.degree)
+	}
+}
+
+func TestGetKey(t *testing.T) {
+	expected := 4
+	n := MakeNode(expected)
+	actual := n.GetKey()
+
+	if actual != expected {
+		t.Errorf("Expected: %d, got: %d", expected, actual)
+	}
+
+	n = &Node{}
+	actual = n.GetKey()
+	if actual != 0 {
+		t.Errorf("Empty node's key should be equal to 0, got: %d", actual)
+	}
+}
+
+func TestAddNode(t *testing.T) {
+	n1 := MakeNode(1)
+	n1.left = n1
+	n1.right = n1
+	n2 := MakeNode(2)
+	n2.left = n2
+	n2.right = n2
+
+	addNode(n1, n2)
+	if n1.left != n2 || n2.right != n1 {
+		t.Error("AddNode should add second node on the left of the first one")
+	}
+}
+
+func TestAddRoot(t *testing.T) {
+	h := MakeHeap()
+	n1 := MakeNode(2)
+	n2 := MakeNode(4)
+	n3 := MakeNode(1)
+
+	t.Run("addRoot on empty Fibonacci Heap", func(t *testing.T) {
+		h.addRoot(n1)
+		if h.min != n1 {
+			t.Error("addRoot should make new node assigned to h.min")
+		}
+	})
+
+	t.Run("addRoot with node's value bigger than h.min's key", func(t *testing.T) {
+		h.addRoot(n2)
+		if h.min == n2 {
+			t.Error("addRoot should leave the old h.min")
+		}
+
+		if h.min.left != n2 {
+			t.Error("addRoot should add the root on the left h.min")
+		}
+	})
+
+	t.Run("addRoot with node's value lesser than the h.min's key", func(t *testing.T) {
+		old := h.min
+		h.addRoot(n3)
+
+		if old.left != n3 {
+			t.Error("addRoot should add the root on the left of previous h.min")
+		}
+
+		if h.min != n3 {
+			t.Error("addRoot should make new node's assigned to h.min")
+		}
+	})
+}
+
+func TestRemoveNodeFromList(t *testing.T) {
+	h := MakeHeap()
+	n1 := MakeNode(2)
+	n2 := MakeNode(4)
+
+	h.addRoot(n1)
+	h.addRoot(n2)
+
+	removeNodeFromList(n1)
+
+	if n2.left == n1 || n1.right == n1 {
+		t.Error("removeNodeFromList should remove connections to node from the list it was in")
+	}
+
+	if n1.left != n2 || n1.right != n2 {
+		t.Error("removeNodeFromList shouldn't modify node's pointers")
+	}
+
+	removeNodeFromList(n2)
+
+	if n2.left != n2 || n2.right != n2 {
+		t.Error("removeNodeFromList should't be able to remove node from list if it's the only element in the list")
+	}
+}
+
+func TestInsert(t *testing.T) {
+	h := MakeHeap()
+	n := MakeNode(3)
+
+	t.Run("Insert on empty Heap", func(t *testing.T) {
+		n.degree = 2
+		n.mark = true
+		n.parent = n
+		n.child = n
+
+		h.Insert(n)
+
+		if n.degree != 0 {
+			t.Errorf("Node's degree should equal to 0, got %d", n.degree)
+		}
+
+		if n.mark != false {
+			t.Error("Node should be unmarked")
+		}
+
+		if n.parent != nil {
+			t.Error("Node's parent should be nil")
+		}
+
+		if n.child != nil {
+			t.Error("Node's child should be nil")
+		}
+
+		if h.min != n {
+			t.Error("Insert should make node assigned to h.min")
+		}
+
+		if h.n != 1 {
+			t.Errorf("Heaps number of nodes should increment to 1, got %d", h.n)
+		}
+	})
+}
+
+func TestMinimum(t *testing.T) {
+	h := MakeHeap()
+	n1 := MakeNode(4)
+	n2 := MakeNode(2)
+
+	if h.Minimum() != nil {
+		t.Error("Minimum on empty heap should return nil")
+	}
+
+	h.Insert(n1)
+
+	if h.Minimum() != n1 {
+		t.Error("Minimum should return h.min from heap with single element")
+	}
+
+	h.Insert(n2)
+
+	if h.Minimum() != n2 {
+		t.Error("Minimum should return h.min from updated heap")
+	}
+}
+
+func TestUnion1(t *testing.T) {
+	h1 := MakeHeap()
+	n1 := MakeNode(4)
+	n2 := MakeNode(2)
+	h1.Insert(n1)
+	h1.Insert(n2)
+
+	h2 := MakeHeap()
+	n3 := MakeNode(5)
+	n4 := MakeNode(8)
+	h2.Insert(n3)
+	h2.Insert(n4)
+
+	h3 := h1.Union(h2)
+
+	if h3.n != h1.n+h2.n {
+		t.Errorf("Merged heap's number of nodes should be %d, got %d", h1.n+h2.n, h3.n)
+	}
+
+	if h3.min != n2 {
+		t.Error("Merged heap's min should be the minimum of two heaps")
+	}
+}
+
+func TestUnion2(t *testing.T) {
+	h1 := MakeHeap()
+	n1 := MakeNode(4)
+	n2 := MakeNode(2)
+	h1.Insert(n1)
+	h1.Insert(n2)
+
+	h2 := MakeHeap()
+	n3 := MakeNode(1)
+	n4 := MakeNode(8)
+	h2.Insert(n3)
+	h2.Insert(n4)
+
+	h3 := h1.Union(h2)
+
+	if h3.min != n3 {
+		t.Error("Merged heap's min should be the minimum of two heaps")
+	}
+}
+
+func TestLink(t *testing.T) {
+	n1 := MakeNode(1)
+	n1.left = n1
+	n1.right = n1
+	n2 := MakeNode(3)
+	n2.left = n2
+	n2.right = n2
+
+	n3 := MakeNode(5)
+	n3.left = n3
+	n3.right = n3
+
+	t.Run("link to node without child", func(t *testing.T) {
+		link(n1, n2)
+		if n1.parent != n2 {
+			t.Error("link should assign node's parent")
+		}
+
+		if n1.mark != false {
+			t.Error("link should unmark the node")
+		}
+
+		if n1.left != n1 || n1.right != n1 {
+			t.Error("link should create list with single element")
+		}
+	})
+
+	t.Run("link to node with a child", func(t *testing.T) {
+		link(n3, n2)
+
+		if n2.child.left != n3 {
+			t.Error("link should add node on the left side of parent's child")
+		}
+	})
+}
+
+func TestConsolidate(t *testing.T) {
+	h := MakeHeap()
+	n1 := MakeNode(1)
+	n1.left = n1
+	n1.right = n1
+	h.Insert(n1)
+	n2 := MakeNode(2)
+	n2.left = n2
+	n2.right = n2
+	h.Insert(n2)
+
+	h.consolidate()
+	if h.Minimum() != n1 {
+		t.Error("consolidate should update heaps mininum element")
+	}
+
+	if h.min.child != n2 {
+		t.Error("consolidate should make heap's root list contain only single root with given degree")
+	}
+}
+
+func TestExtractMin(t *testing.T) {
+	h := MakeHeap()
+	n1 := MakeNode(1)
+	n1.left = n1
+	n1.right = n1
+	h.Insert(n1)
+	n2 := MakeNode(2)
+	n2.left = n2
+	n2.right = n2
+	h.Insert(n2)
+	n3 := MakeNode(3)
+	n3.left = n3
+	n3.right = n3
+	h.Insert(n3)
+
+	n4 := h.ExtractMin()
+
+	if n4 != n1 {
+		t.Error("ExtractMin should extract minimum from the heap")
+	}
+
+	if h.Minimum() != n2 {
+		t.Error("ExtractMin should update minimum of the heap")
+	}
+
+	if h.n != 2 {
+		t.Errorf("number of nodes in the heap should be equal to 2, got %d", h.n)
+	}
+
+	n5 := MakeNode(5)
+	n5.right = n5
+	n5.left = n5
+	addNode(n3, n5)
+
+	h.ExtractMin()
+	h.ExtractMin()
+	h.ExtractMin()
+
+	if h.min != nil {
+		t.Error("ExtractMin should leave nil as h.min if there was only one element in the root list")
+	}
+}
+
+func TestErrInvalidArgument(t *testing.T) {
+	expected := "Cannot decrese key to a bigger value: 4"
+	actual := errInvalidArgument(4).Error()
+
+	if expected != actual {
+		t.Error("ErrInvalidArgument should rerturn proper message")
+	}
+}
+
+func TestCut(t *testing.T) {
+	h := MakeHeap()
+	n1 := MakeNode(2)
+	n1.left = n1
+	n1.right = n1
+	h.Insert(n1)
+	n2 := MakeNode(4)
+	addNode(n1, n2)
+	n1.degree = 1
+
+	h.cut(n2, n1)
+
+	if n1.degree != 0 {
+		t.Errorf("Roots degree should be equal to 0, got: %d", n1.degree)
+	}
+
+	if n2.mark != false {
+		t.Error("cut should unmark the node")
+	}
+
+	if n2.parent != nil {
+		t.Error("cut should make node's parent equal to nil")
+	}
+}
+
+func TestCascadingCut(t *testing.T) {
+	h := MakeHeap()
+	n1 := MakeNode(2)
+	n1.left = n1
+	n1.right = n1
+	h.Insert(n1)
+	n2 := MakeNode(3)
+	n2.left = n2
+	n2.right = n2
+	link(n2, n1)
+
+	h.cascadingCut(n2)
+
+	if n2.mark != true {
+		t.Error("cascadingCut should mark unmarked node")
+	}
+
+	h.cascadingCut(n2)
+
+	if n2.parent != nil {
+		t.Error("cascadingCut should remove parent if node was marked")
+	}
+}
+
+func TestDecreaseKey(t *testing.T) {
+	h := MakeHeap()
+	n1 := MakeNode(2)
+	n1.left = n1
+	n1.right = n1
+	h.Insert(n1)
+	t.Run("with argument greater than the previous key", func(t *testing.T) {
+		err := h.DecreaseKey(n1, 5)
+		if _, ok := err.(errInvalidArgument); !ok {
+			t.Error("should return errInvalidArgument error")
+		}
+	})
+
+	n2 := MakeNode(4)
+	n2.left = n2
+	n2.right = n2
+	link(n2, n1)
+	t.Run("with valid argument", func(t *testing.T) {
+		expectedKey := 1
+		err := h.DecreaseKey(n2, expectedKey)
+		if err != nil {
+			t.Error("shouldn't return an error")
+		}
+
+		if n2.key != expectedKey {
+			t.Errorf("Expected updated node's key to equal %d, got %d", expectedKey, n2.key)
+		}
+
+		if h.min != n2 {
+			t.Error("should update heap's mean")
+		}
+	})
+}
+
+func TestDelete(t *testing.T) {
+	h := MakeHeap()
+	n1 := MakeNode(2)
+	n1.left = n1
+	n1.right = n1
+	h.Insert(n1)
+
+	err := h.Delete(n1)
+	if err != nil {
+		t.Error("shouldn't return an error")
+	}
+
+	if h.Minimum() != nil {
+		t.Error("should remove node from the heap")
+	}
+}
