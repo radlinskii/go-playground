233cd6215d9cb1f9c8a18834941913307a99ee23
radlinskii <radlinskiignacy@gmail.com>
2018-10-14 19:14:04 +0200

    Rename utils/fileutils package to utils/file

---
+++ utils/file/doc.go
@@ -0,0 +1,2 @@
+// Package file contains utilities that might be useful in working with files.
+package file
---
+++ utils/file/remove_tabs.go
@@ -0,0 +1,23 @@
+package file
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+// RemoveTabs removes tabulation character from specified file and replaces them with 4 space character.
+func RemoveTabs(filename string) (err error) {
+	input, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	output := strings.Replace(string(input), "\t", "    ", -1)
+
+	err = ioutil.WriteFile(filename, []byte(output), 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return
+}
