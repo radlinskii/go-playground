c906b78db7f4b6ff2d177ddffb371397932d1eec
Radliński Ignacy <radlinskiignacy@gmail.com>
2018-10-21 22:35:33 +0200

    Algo (#14)
    * Update files structure
    
    * Implement Eratosthenes Sieve
    
    * Create heap sort algorithm implementation
    
    Used Fibonacci Heap data structure to create a heap sort easily.

---
+++ algo/eratosthenes_sieve.go
@@ -0,0 +1,29 @@
+package algo
+
+// EratosthenesSieve is an implementation of Sieve of Eratosthenes.
+// EratosthenesSieve returns slice of all primes numbers that are lower than given number.
+func EratosthenesSieve(n int) []int {
+	if n < 3 {
+		return []int{}
+	}
+
+	numbers := make([]bool, n)
+	numbers[0] = true
+	numbers[1] = true
+
+	for i := 2; i*i <= n; i++ {
+		if !numbers[i] {
+			for j := i * i; j < n; j += i {
+				numbers[j] = true
+			}
+		}
+	}
+
+	var primes []int
+	for i := 0; i < n; i++ {
+		if !numbers[i] {
+			primes = append(primes, i)
+		}
+	}
+	return primes
+}
