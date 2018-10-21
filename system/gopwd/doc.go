01d5978273280ec6c6c74d581eebb6eccea70247
radlinskii <radlinskiignacy@gmail.com>
2018-10-21 23:48:35 +0200

    Update gopwd documentation

---
+++ system/gopwd/doc.go
@@ -0,0 +1,13 @@
+/*
+Gopwd is an implementation of pwd(1) command.
+
+Usage:
+	gopwd [flag]
+
+The flags are:
+	-L:		Displays the current logical working directory.
+	-P: 	Displays the current physical working directory(all symbolic links resolved).
+
+*/
+
+package main
