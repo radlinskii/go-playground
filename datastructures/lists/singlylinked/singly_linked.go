a43c4af9991428b839a386c9185351bc02e5c7b4
Radliński Ignacy <radlinskiignacy@gmail.com>
2018-11-02 10:17:29 +0100

    Singly linked list (#17)
    * Create scaffold of singly linked list data structure
    
    * Complete basic setup of singly linked list data structure
    
    * Implement Print and Insert method of singly linked list
    
    * Implement HasCycle method of singly linked list
    
    * Implement Reverse method of singly linked list
    
    * Implement Compare method for singly linked list
    
    * Implement InsertSort method for singly linked list
    
    * Implement RemoveDuplicatesFromSortedList method
    
    * Implement IsAscending method for singly linked list

---
+++ datastructures/lists/singlylinked/singly_linked.go
@@ -0,0 +1,233 @@
+package singlylinked
+
+import "fmt"
+
+// Node type is a node of singly linked list.
+type Node struct {
+	key  int
+	next *Node
+}
+
+func makeNode(v int) *Node {
+	return &Node{key: v, next: nil}
+}
+
+// List is a structure holding singly linked list.
+type List struct {
+	head *Node
+}
+
+// MakeList creates an empty list.
+func MakeList() *List {
+	return &List{}
+}
+
+// Print prints keys of elements of the list.
+func (l *List) Print() {
+	x := l.head
+	if x == nil {
+		fmt.Println("Empty list!")
+	} else {
+		fmt.Print("->")
+		for x != nil {
+			fmt.Printf("%d->", x.key)
+			x = x.next
+		}
+		fmt.Println()
+	}
+}
+
+// Prepend is adding node with a given value at the head of the list.
+func (l *List) Prepend(v int) {
+	n := makeNode(v)
+
+	n.next = l.head
+	l.head = n
+}
+
+// Append should add an item at the end of the list.
+func (l *List) Append(v int) {
+	x := l.head
+	if x == nil {
+		l.Prepend(v)
+	} else {
+		for x.next != nil {
+			x = x.next
+		}
+		n := makeNode(v)
+		x.next = n
+	}
+}
+
+// Pop is removing first element from the list and returns it.
+func (l *List) Pop() *Node {
+	x := l.head
+	if l.head != nil {
+		l.head = x.next
+	}
+	return x
+}
+
+// Search finds the first element with given value as its key.
+func (l *List) Search(v int) *Node {
+	x := l.head
+	for x != nil && x.key != v {
+		x = x.next
+	}
+	return x
+}
+
+// Delete removes from a list first node of a given value.
+// It returns true if node was deleted, false otherwise.
+func (l *List) Delete(v int) bool {
+	x := l.head
+	if x == nil {
+		return false
+	}
+	if x.key == v {
+		l.head = x.next
+		return true
+	}
+	for x.next != nil {
+		if x.next.key == v {
+			x.next = x.next.next
+			return true
+		}
+		x = x.next
+	}
+	return false
+}
+
+// Insert inserts a node with given value at given position.
+// It returns true if node was inserted, false otherwise.
+func (l *List) Insert(v, pos int) bool {
+	if pos < 0 {
+		return false
+	}
+	if pos == 0 {
+		l.Prepend(v)
+		return true
+	}
+	i := 1
+	x := l.head
+	for x.next != nil && i != pos {
+		x = x.next
+		i++
+	}
+	if i != pos {
+		return false
+	}
+	n := makeNode(v)
+	n.next = x.next
+	x.next = n
+	return true
+}
+
+// InsertSort inserts a node with given values inside the list
+// in such position that keeps list in ascending order.
+func (l *List) InsertSort(v int) {
+	if l.head == nil || l.head.key > v {
+		l.Prepend(v)
+		return
+	}
+	x := l.head
+	for x.next != nil && x.next.key < v {
+		x = x.next
+	}
+	n := makeNode(v)
+	n.next = x.next
+	x.next = n
+}
+
+// HasCycle checks if list contains a cycle.
+func (l *List) HasCycle() bool {
+	if l.head == nil {
+		return false
+	}
+
+	slow := l.head
+	fast := l.head.next
+	for slow != fast {
+		if fast == nil || fast.next == nil {
+			return false
+		}
+		slow = slow.next
+		fast = fast.next.next
+	}
+	return true
+}
+
+// Reverse reverses the order of the list.
+func (l *List) Reverse() {
+	var prev *Node
+	current := l.head
+	var next *Node
+	for current != nil {
+		next = current.next
+		current.next = prev
+		prev = current
+		current = next
+	}
+	l.head = prev
+}
+
+// Compare checks equality of two lists.
+func (l *List) Compare(l2 *List) bool {
+	if l.head == nil {
+		if l2.head == nil {
+			return true
+		}
+		return false
+	}
+	if l2.head == nil {
+		return false
+	}
+	x1 := l.head
+	x2 := l2.head
+	for x1 != nil && x2 != nil {
+		if x1.key != x2.key {
+			return false
+		}
+		x1 = x1.next
+		x2 = x2.next
+	}
+	if (x1 == nil && x2 != nil) || (x1 != nil && x2 == nil) {
+		return false
+	}
+	return true
+}
+
+// IsAscending checks if list is sorted in ascending order.
+func (l *List) IsAscending() bool {
+	if l.head == nil || l.head.next == nil {
+		return true
+	}
+	x := l.head
+	for x.next != nil {
+		if x.next.key < x.key {
+			return false
+		}
+		x = x.next
+	}
+	return true
+}
+
+// RemoveDuplicatesFromAscendingList removes nodes with duplicated values.
+func (l *List) RemoveDuplicatesFromAscendingList() {
+	if l.head == nil || l.head.next == nil {
+		return
+	}
+	x := l.head
+	for x.next != nil {
+		if x.key == x.next.key {
+			x.next = x.next.next
+		}
+		if x.next != nil {
+			x = x.next
+		}
+	}
+}
+
+// MergeAscendingLists
+
+// SortAscending
---
+++ datastructures/lists/singlylinked/singlylinked_test.go
@@ -0,0 +1,335 @@
+package singlylinked
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMakeNode(t *testing.T) {
+	var testsTable = []struct {
+		name     string
+		key      int
+		expected int
+	}{
+		{"negative number", -321, -321},
+		{"zero", 0, 0},
+		{"positive number", 321, 321},
+	}
+
+	for _, test := range testsTable {
+		t.Run(test.name, func(t *testing.T) {
+			got := makeNode(test.key)
+			if got.key != test.expected {
+				t.Errorf("makeNode(%d): expected: %d got %d", test.key, got.key, test.expected)
+			}
+
+			if got.next != nil {
+				t.Error("makeNode should produce node with nil as its next property")
+			}
+		})
+	}
+}
+
+func TestMakeList(t *testing.T) {
+	l := MakeList()
+	if l.head != nil {
+		t.Error("MakeList should create an empty list")
+	}
+}
+
+func TestPrepend(t *testing.T) {
+	l := MakeList()
+	l.Prepend(3)
+
+	if l.head.key != 3 {
+		t.Error("Prepend should insert 3 as key of list head")
+	}
+
+	l.Prepend(5)
+	if l.head.key != 5 {
+		t.Error("Prepend should insert 5 as key of list head")
+	}
+
+	if l.head.next.key != 3 {
+		t.Error("Prepend add new node at the begging of the list")
+	}
+}
+
+func TestPop(t *testing.T) {
+	l := MakeList()
+	n := l.Pop()
+	if n != nil {
+		t.Error("Pop should return nil when called on an empty list")
+	}
+
+	l.Prepend(4)
+
+	n = l.Pop()
+	if n.key != 4 {
+		t.Error("Pop should return first element from a list")
+	}
+
+	if l.head != nil {
+		t.Error("Pop should remove an element from a list")
+	}
+}
+
+func TestSearch(t *testing.T) {
+	l := MakeList()
+
+	if l.Search(3) != nil {
+		t.Error("Search on empty list should definitely return nil")
+	}
+
+	l.Prepend(3)
+
+	if l.Search(3) != l.head {
+		t.Error("Search should find the right node in list with single element")
+	}
+
+	l.Prepend(4)
+	if l.Search(3) != l.head.next {
+		t.Error("Search should find nodes deep in the list")
+	}
+
+	if l.Search(5) != nil {
+		t.Error("Search should not find unexisting values")
+	}
+}
+
+func TestDelete(t *testing.T) {
+	l := MakeList()
+	if l.Delete(3) {
+		t.Error("Delete should return false when called on empty list")
+	}
+	l.Prepend(3)
+	l.Prepend(4)
+	l.Prepend(5)
+
+	if !l.Delete(3) {
+		t.Error("Delete should return true when removing an element")
+	}
+	if l.head.next.next != nil {
+		t.Error("Delete should remove node from a list")
+	}
+	if l.head.key != 5 {
+		t.Error("Delete should not change rest of the elements")
+	}
+
+	l.Delete(5)
+	if l.head.key != 4 {
+		t.Error("Delete should remove first element from the list")
+	}
+
+	l.Delete(4)
+
+	if l.head != nil {
+		t.Error("Delete should remove last element from the list")
+	}
+}
+
+func TestAppend(t *testing.T) {
+	l := MakeList()
+	l.Append(4)
+
+	if l.head.key != 4 {
+		t.Error("Append should at the first item to the list")
+	}
+
+	l.Append(5)
+	if l.head.next.key != 5 {
+		t.Error("Append should add a node at the end of the list")
+	}
+
+	if l.head.key != 4 {
+		t.Error("Append should not update list head")
+	}
+}
+
+func ExamplePrint() {
+	l := MakeList()
+	l.Print()
+	l.Prepend(3)
+	l.Print()
+	l.Prepend(4)
+	l.Print()
+	l.Prepend(5)
+	l.Print()
+
+	// Output:
+	// Empty list!
+	// ->3->
+	// ->4->3->
+	// ->5->4->3->
+}
+
+func ExampleInsert() {
+	l := MakeList()
+	l.Insert(4, 0)
+	l.Print()
+	fmt.Println(l.Insert(4, 2))
+	l.Print()
+	l.Insert(5, 1)
+	l.Print()
+	l.Insert(6, 2)
+	l.Print()
+	l.Insert(3, 1)
+	l.Print()
+	l.Insert(8, 0)
+	l.Print()
+
+	// Output:
+	// ->4->
+	// false
+	// ->4->
+	// ->4->5->
+	// ->4->5->6->
+	// ->4->3->5->6->
+	// ->8->4->3->5->6->
+}
+
+func TestHasCycle1(t *testing.T) {
+	l := MakeList()
+	if l.HasCycle() {
+		t.Error("HasCycle shouldn't detect a cycle without a reason")
+	}
+	l.Prepend(2)
+	l.Prepend(3)
+	l.Prepend(4)
+
+	if l.HasCycle() {
+		t.Error("HasCycle shouldn't detect a cycle without a reason")
+	}
+}
+
+func TestHasCycle2(t *testing.T) {
+	l := MakeList()
+
+	l.Prepend(3)
+	l.Prepend(4)
+	l.Prepend(5)
+	n := l.Search(3)
+	n.next = l.head
+
+	if !l.HasCycle() {
+		t.Error("HasCycle should properly detect a cycle")
+	}
+}
+
+func ExampleReverse() {
+	l := MakeList()
+	l.Prepend(4)
+	l.Prepend(5)
+	l.Prepend(7)
+	l.Prepend(2)
+	l.Print()
+	l.Reverse()
+	l.Print()
+
+	// Output:
+	// ->2->7->5->4->
+	// ->4->5->7->2->
+}
+
+func ExampleCompare() {
+	l := MakeList()
+	l2 := MakeList()
+	fmt.Println(l.Compare(l2))
+	l.Append(4)
+	fmt.Println(l.Compare(l2))
+	l2.Append(4)
+	fmt.Println(l.Compare(l2))
+	l.Append(5)
+	fmt.Println(l.Compare(l2))
+	l2.Append(5)
+	fmt.Println(l.Compare(l2))
+	l2.Append(7)
+	l.Append(8)
+	fmt.Println(l.Compare(l2))
+
+	// Output:
+	// true
+	// false
+	// true
+	// false
+	// true
+	// false
+}
+
+func ExampleInsertSort() {
+	l := MakeList()
+
+	l.InsertSort(4)
+	l.Print()
+	l.InsertSort(7)
+	l.Print()
+	l.InsertSort(3)
+	l.Print()
+	l.InsertSort(8)
+	l.Print()
+	l.InsertSort(0)
+	l.Print()
+	l.InsertSort(5)
+	l.Print()
+
+	// Output:
+	// ->4->
+	// ->4->7->
+	// ->3->4->7->
+	// ->3->4->7->8->
+	// ->0->3->4->7->8->
+	// ->0->3->4->5->7->8->
+}
+
+func ExampleRemoveDuplicatesFromAscendingList() {
+	l := MakeList()
+
+	l.RemoveDuplicatesFromAscendingList()
+	l.Print()
+	l.InsertSort(3)
+	l.RemoveDuplicatesFromAscendingList()
+	l.Print()
+	l.InsertSort(3)
+	l.Print()
+	l.RemoveDuplicatesFromAscendingList()
+	l.Print()
+	l.InsertSort(7)
+	l.RemoveDuplicatesFromAscendingList()
+	l.Print()
+	l.InsertSort(3)
+	l.Print()
+	l.RemoveDuplicatesFromAscendingList()
+	l.Print()
+
+	// Output:
+	// Empty list!
+	// ->3->
+	// ->3->3->
+	// ->3->
+	// ->3->7->
+	// ->3->3->7->
+	// ->3->7->
+}
+
+func ExampleIsAscending() {
+	l := MakeList()
+	fmt.Println(l.IsAscending())
+	l.Prepend(2)
+	fmt.Println(l.IsAscending())
+	l.Prepend(4)
+	fmt.Println(l.IsAscending())
+	l.Prepend(1)
+	fmt.Println(l.IsAscending())
+	l.Delete(4)
+	fmt.Println(l.IsAscending())
+	l.Append(5)
+	fmt.Println(l.IsAscending())
+
+	// Output:
+	// true
+	// true
+	// false
+	// false
+	// true
+	// true
+}
