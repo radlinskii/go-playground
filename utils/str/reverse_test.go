790cf7ff47ac8cbb485491fd9c276fa85285af02
radlinskii <radlinskiignacy@gmail.com>
2018-11-03 13:29:06 +0100

    Implement recursive method of reversing a string

--- utils/str/reverse_test.go
+++ utils/str/reverse_test.go
@@ -24,3 +24,26 @@ func TestReverse(t *testing.T) {
 		})
 	}
 }
+
+func TestReverseRecursively(t *testing.T) {
+	var testsTable = []struct {
+		name string
+		string
+		expected string
+	}{
+		{"empty string", "", ""},
+		{"word", "test", "tset"},
+		{"palindrome", "level", "level"},
+		{"string with spaces", "fat cat", "tac taf"},
+		{"string with non-alphabetic characters", "#1337!!1", "1!!7331#"},
+	}
+
+	for _, test := range testsTable {
+		t.Run(test.name, func(t *testing.T) {
+			got := ReverseRecursively(test.string)
+			if got != test.expected {
+				t.Errorf("\nReverseRecursively(%s) \nexpected: %s \ngot: %s", test.string, test.expected, got)
+			}
+		})
+	}
+}
