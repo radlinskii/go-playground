8cfc655f1f8db88842b688602d2859a69a974efd
radlinskii <radlinskiignacy@gmail.com>
2018-11-08 13:38:09 +0100

    Implement stack data structure using singly linked list

---
+++ datastructures/stack.go
@@ -0,0 +1,43 @@
+package datastructures
+
+import (
+	"github.com/radlinskii/go-playground/datastructures/lists/singlylinked"
+)
+
+// Stack type is a implementation of stack data structure.
+// It uses singly linked list datastructure and its functionalities.
+type Stack struct {
+	list *singlylinked.List
+}
+
+// MakeStack creates an empty stack.
+func MakeStack() *Stack {
+	return &Stack{list: singlylinked.MakeList()}
+}
+
+// Push inserts an element of given value at the top of the stack.
+func (s *Stack) Push(v int) {
+	s.list.Prepend(v)
+}
+
+// Pop returns an element from the top after removing it from the stack.
+func (s *Stack) Pop() *singlylinked.Node {
+	return s.list.Pop()
+}
+
+// IsEmpty checks if top of the stack is empty.
+func (s *Stack) IsEmpty() bool {
+	return s.list.IsEmpty()
+}
+
+// Top returns first element from the stack witthout removing it.
+func (s *Stack) Top() *singlylinked.Node {
+	return s.list.GetHead()
+}
+
+func (s *Stack) String() string {
+	if s.list.IsEmpty() {
+		return "Empty stack!"
+	}
+	return s.list.String()
+}
