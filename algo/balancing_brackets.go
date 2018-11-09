4e780af98fd6c32a4c4d120cc46055a6b8ef1e51
radlinskii <radlinskiignacy@gmail.com>
2018-11-09 10:41:02 +0100

    Update balancing brackets algorightm to use stack data structure

--- algo/balancing_brackets.go
+++ algo/balancing_brackets.go
@@ -1,41 +1,39 @@
 package algo
 
-import (
-	"github.com/radlinskii/go-playground/datastructures/lists/singlylinked"
-)
+import "github.com/radlinskii/go-playground/datastructures"
 
 // IsBalanced takes a string containing brackets
 // and checks if the brackets are in balanced order.
 func IsBalanced(s string) bool {
-	list := singlylinked.MakeList()
+	stack := datastructures.MakeStack()
 	if len(s) == 0 {
 		return true
 	}
 	if len(s) == 1 {
 		return false
 	}
-	list.Prepend(int(s[0]))
+	stack.Push(int(s[0]))
 	for _, v := range s[1:len(s)] {
 		if v == 123 || v == 40 || v == 91 {
-			list.Prepend(int(v))
+			stack.Push(int(v))
 		} else {
 			if v == 125 { // }
-				if list.IsEmpty() || list.GetHead().GetKey() != 123 { // {
+				if stack.IsEmpty() || stack.Top().GetKey() != 123 { // {
 					return false
 				}
-				list.Pop()
+				stack.Pop()
 			} else if v == 41 { // )
-				if list.IsEmpty() || list.GetHead().GetKey() != 40 { // (
+				if stack.IsEmpty() || stack.Top().GetKey() != 40 { // (
 					return false
 				}
-				list.Pop()
+				stack.Pop()
 			} else if v == 93 { // ]
-				if list.IsEmpty() || list.GetHead().GetKey() != 91 { // [
+				if stack.IsEmpty() || stack.Top().GetKey() != 91 { // [
 					return false
 				}
-				list.Pop()
+				stack.Pop()
 			}
 		}
 	}
-	return list.IsEmpty()
+	return stack.IsEmpty()
 }
