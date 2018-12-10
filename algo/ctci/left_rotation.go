8dbac8980c6cc23142a182a918cbe4e148b295ff
radlinskii <radlinskiignacy@gmail.com>
2018-12-10 22:25:21 +0100

    LeftRotate cleanup

--- algo/ctci/left_rotation.go
+++ algo/ctci/left_rotation.go
@@ -5,13 +5,11 @@ func LeftRotate(arr [][]int) {
 	if len(arr) > 1 {
 		for i := 0; i <= len(arr)/2; i++ {
 			for j := i; j+i < len(arr)-1; j++ {
-				tmp1 := arr[i][j]
+				tmp := arr[i][j]
 				arr[i][j] = arr[len(arr)-1-j][i]
-				tmp2 := arr[j][len(arr)-1-i]
-				arr[j][len(arr)-1-i] = tmp1
-				tmp1 = arr[len(arr)-1-i][len(arr)-1-j]
-				arr[len(arr)-1-i][len(arr)-1-j] = tmp2
-				arr[len(arr)-1-j][i] = tmp1
+				arr[len(arr)-1-j][i] = arr[len(arr)-1-i][len(arr)-1-j]
+				arr[len(arr)-1-i][len(arr)-1-j] = arr[j][len(arr)-1-i]
+				arr[j][len(arr)-1-i] = tmp
 			}
 		}
 	}
