2aa6aa18fb3f4e9f7670bbb9d9d2c65540edb34b
radlinskii <radlinskiignacy@gmail.com>
2019-02-05 19:49:59 +0100

    simple session secure route

--- myhttp/sessions/simple_sessions.go
+++ myhttp/sessions/simple_sessions.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"time"
@@ -9,26 +9,77 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
+type user struct {
+	UserName string
+	First    string
+	Last     string
+}
+
+var tpl *template.Template
+var dbUsers = map[string]user{}      // user ID, user
+var dbSessions = map[string]string{} // session ID, user ID
+
+func init() {
+	tpl = template.Must(template.ParseGlob("myhttp/sessions/templates/*"))
+}
+
 func main() {
-	http.HandleFunc("/", home)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/bar", bar)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func home(w http.ResponseWriter, req *http.Request) {
-	cookie, err := req.Cookie("session")
+func index(w http.ResponseWriter, req *http.Request) {
+
+	// get cookie
+	c, err := req.Cookie("session")
 	if err != nil {
-		if err == http.ErrNoCookie {
-			t := time.Now()
-			id := uuid.NewV4()
-			cookie = &http.Cookie{Name: "session", Value: id.String(), Expires: t.Add(30 * 24 * time.Hour)}
-			http.SetCookie(w, cookie)
-		} else {
-			http.Error(w, "Server Error!", http.StatusInternalServerError)
-			_ = fmt.Errorf("Server Error: %s", err.Error())
-			return
+		sID := uuid.NewV4()
+		c = &http.Cookie{
+			Name:    "session",
+			Value:   sID.String(),
+			Expires: time.Now().Add(30 * time.Second),
 		}
+		http.SetCookie(w, c)
 	}
-	fmt.Fprintln(w, cookie)
+
+	// if the user exists already, get user
+	var u user
+	if un, ok := dbSessions[c.Value]; ok {
+		u = dbUsers[un]
+	}
+
+	// process form submission
+	if req.Method == http.MethodPost {
+		un := req.FormValue("username")
+		f := req.FormValue("firstname")
+		l := req.FormValue("lastname")
+		u = user{un, f, l}
+		dbSessions[c.Value] = un
+		dbUsers[un] = u
+	}
+
+	tpl.ExecuteTemplate(w, "index.gohtml", u)
+}
+
+func bar(w http.ResponseWriter, req *http.Request) {
+	// get cookie
+	c, err := req.Cookie("session")
+	if err != nil {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	// if user does not exist in db
+	un, ok := dbSessions[c.Value]
+	if !ok {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	// let the user to the bar
+	u := dbUsers[un]
+	tpl.ExecuteTemplate(w, "bar.gohtml", u)
 }
