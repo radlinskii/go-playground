19a630c4168fa8451635f4b9d978dc2497c1c794
radlinskii <radlinskiignacy@gmail.com>
2019-02-24 16:18:14 +0100

    add getonebyid and createone routes

--- myhttp/postgres/simple_postgres.go
+++ myhttp/postgres/simple_postgres.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	_ "github.com/lib/pq"
 )
@@ -38,23 +39,92 @@ type author struct {
 
 func main() {
 	defer db.Close()
-	http.HandleFunc("/api/v1/authors", getAuthors)
+	http.HandleFunc("/api/v1/authors", authors)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func getAuthors(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		log.Println("error: authors ", http.StatusMethodNotAllowed)
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+func authors(w http.ResponseWriter, r *http.Request) {
+	switch {
+	case r.Method == http.MethodGet:
+		val := r.FormValue("id")
+		if val == "" {
+			getAllAuthors(w, r)
+			return
+		}
+
+		id, err := strconv.Atoi(val)
+		if err != nil {
+			log.Print(err)
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		getAuthorByID(w, r, id)
+		return
+	case r.Method == http.MethodPost:
+		createAuthor(w, r)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	q := `SELECT * from authors where age < $1 ;`
+	log.Println("error: authors ", http.StatusMethodNotAllowed)
+	http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	return
+}
 
-	rows, err := db.Query(q, 27)
+//  curl -i -X POST -d "id=4&firstName=marcelina&lastName=radlinska&phoneNumber=+4865423322&age=25&description=author number 4" localhost:8080/api/v1/authors
+func createAuthor(w http.ResponseWriter, r *http.Request) {
+	a := author{}
+	ageval := r.FormValue("age")
+	a.Fname = r.FormValue("firstName")
+	a.Lname = r.FormValue("lastName")
+	a.Phone = r.FormValue("phoneNumber")
+	a.Description = r.FormValue("description")
+
+	if a.Fname == "" || a.Lname == "" || a.Phone == "" || a.Description == "" || ageval == "" {
+		http.Error(w, http.StatusText(http.StatusNotAcceptable), http.StatusNotAcceptable)
+		return
+	}
+
+	age, err := strconv.Atoi(ageval)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, http.StatusText(http.StatusNotAcceptable), http.StatusNotAcceptable)
+		return
+	}
+
+	a.Age = age
+
+	result, err := db.Exec(`INSERT INTO authors
+	(first_name, last_name, phone_number, age, description) values
+	($1, $2, $3, $4, $5);`, a.Fname, a.Lname, a.Phone, a.Age, a.Description)
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	rows, err := result.RowsAffected()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	if rows != 1 {
+		log.Println("No rows inserted.")
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getAllAuthors(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	q := `SELECT * from authors;`
+
+	rows, err := db.Query(q)
 	if err != nil {
 		log.Print(err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
@@ -80,6 +150,33 @@ func getAuthors(w http.ResponseWriter, r *http.Request) {
 	}
 
 	err = json.NewEncoder(w).Encode(as)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+}
+
+func getAuthorByID(w http.ResponseWriter, r *http.Request, id int) {
+	w.Header().Set("Content-Type", "application/json")
+	q := `SELECT * from authors where author_id = $1 ;`
+
+	row := db.QueryRow(q, id)
+
+	a := author{}
+	err := row.Scan(&a.ID, &a.Fname, &a.Lname, &a.Phone, &a.Age, &a.Description)
+	switch {
+	case err == sql.ErrNoRows:
+		log.Print(err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	case err != nil:
+		log.Print(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(a)
 	if err != nil {
 		log.Print(err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
